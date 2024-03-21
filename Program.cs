6387ad9a4cda742bbca75c41f2acf2ad6a4fa174
Izabelly Rodrigues <149176367+rdgsizabelly@users.noreply.github.com>
2024-03-20 23:45:05 -0300

    Praticando Vetor e Matriz

--- Program.cs
+++ Program.cs
@@ -1,57 +1,38 @@
 ﻿using System;
 
-class Program
-{
+class Program{
+
     static void Main(){
-        
-        int tempo = 0;
-        char escolha;
 
-        inicio:
+        int a;
+        int[] n = new int[5];
+        int[,] num = new int[2,2],
+        string[] vei = new string[3];
 
-        Console.Clear();
+        vei[0] = "carroça";
+        vei[1] = "carrinho de mão";
+        vei[2] = "escada";
 
-        Console.WriteLine("Recife - João Pessoa");
-        Console.WriteLine("Ecolha o meio de transpote([a] avião/ [c] carro / [o] onibus): ");
+        Console.WriteLine(vei[0]); 
+        num[0,0] = 1;
+        num[0,1] = 2;
+        num[1,0] = 3;
+        num[1,1] = 4;
+        // n[0] = 12;
+        // n[1] = 41;
+        // n[2] = 14;
+        // n[3] = 13;
+        // n[4] = 31;
 
-        escolha = char.Parse(Console.ReadLine());
+        a = int.Parse(Console.ReadLine());
 
-
-        switch (escolha){
-
-            case 'a':
-            case 'A':
-                tempo = 45;
-                break;
-            case 'c':
-            case 'C':
-                tempo = 130;
-                break;
-            case 'o':
-            case 'O':
-                tempo = 2400;
-                break;
-            default:
-            tempo = -1;
-            break;
+        for (int i = 0; i <= 5; i++)
+        {
+            n[i] = a + i;
+            Console.WriteLine(n[i]);
         }
 
-    if(tempo <0){
-        Console.WriteLine("Tempo indisponivel!");
-    }else
-    {
-        Console.WriteLine("Levara {0} para chegar la", tempo);
+        Console.ReadKey();
     }
-    
-    Console.WriteLine("Calcular de novo?[s/n]");
-    escolha = char.Parse(Console.ReadLine());
 
-    if(escolha == 's' || escolha =='S'){
-        goto inicio;
-    }else{
-        Console.Clear();
-        Console.WriteLine("Fim do prog");
-    }
-    Console.ReadKey();
-    }
 }
