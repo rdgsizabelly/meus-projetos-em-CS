e95eb24b9290f79dcaac8a8f763b04a62f3aeb4c
Izabelly Rodrigues <149176367+rdgsizabelly@users.noreply.github.com>
2024-03-23 13:23:04 -0300

    Praticando vetor e matriz

--- Program.cs
+++ Program.cs
@@ -3,67 +3,36 @@
 class Program{
 
     static void Main(){
-        //metodo com retorno 
-         int v1, v2, r;
-         v1 = int.Parse(Console.ReadLine());
-         v2 = int.Parse(Console.ReadLine());
-         r = soma(v1, v2);
-         Console.WriteLine("A soma de {0} e {1} é igual a: {2}", v1, v2, r);
-         //soma(v1, v2);
 
-        //metodo passagem por ref e valor
-         int num = 10;
-         dobrar( ref num);
-         Console.WriteLine(num);
+        int a;
+        int[] n = new int[5];
+        int[,] num = new int[2,2],
+        string[] vei = new string[3];
 
-        //argumento out para retornar mais de um resultado
-        int divid, divis, quoc, rest;
-        divid = 10;
-        divis = 5;
-        quoc= D1 (divid, divis, out rest);
-        Console.WriteLine("{0} / {1} = {2} e o resto = {3}", divid, divis, quoc, rest);
+        vei[0] = "carroça";
+        vei[1] = "carrinho de mão";
+        vei[2] = "escada";
 
-        soma2(23, 32);
-        Console.ReadKey();
-    }
+        Console.WriteLine(vei[0]); 
+        num[0,0] = 1;
+        num[0,1] = 2;
+        num[1,0] = 3;
+        num[1,1] = 4;
+        // n[0] = 12;
+        // n[1] = 41;
+        // n[2] = 14;
+        // n[3] = 13;
+        // n[4] = 31;
 
-    //metodo com retorno
-     static int soma(int n1, int n2){
-         int result = n1 + n2;
-       //Console.WriteLine("A soma de {0} e {1} é igual a: {2}", n1, n2, result);
-        return result;
-     }
+        a = int.Parse(Console.ReadLine());
 
-    //metodo passagem por ref e por valor
-     static void dobrar(ref int valor){
-
-         valor *= 2;
-     }
-
-    //argumento out para retornar mais de um resultado
-    static int D1(int dividendo, int divisor, out int resto){
-        int quociente;
-        quociente= dividendo/divisor;
-        resto= dividendo%divisor;
-        return quociente;
-
-    }
-
-    //argumento params para criar argumentos sem precisar declarar
-    static void soma2(params int[] n){
-        int rest=0;
-
-        if(n.Length < 1){
-            Console.WriteLine("Não existem numero sufucientes");
-        }else if(n.Length < 2){
-            Console.WriteLine("Somente um numero informado. num: {0}", n[0]);
-        }else{
-            for (int i = 0; i < n.Length; i++)
-            {
-                rest += n[i];
-            }
-            Console.WriteLine("A soma dos numero é: {0}", rest);
+        for (int i = 0; i <= 5; i++)
+        {
+            n[i] = a + i;
+            Console.WriteLine(n[i]);
         }
+
+        Console.ReadKey();
     }
 
 }
