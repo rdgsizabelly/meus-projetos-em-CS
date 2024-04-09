dd306ce9848fd93fef4469a9db7b0f6828c9fc8e
Izabelly Rodrigues <149176367+rdgsizabelly@users.noreply.github.com>
2024-04-09 18:48:34 -0300

    Praticando visibilidade

---
+++ aulaPublicPrivate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Jogador{
+    private int energia;
+    private string nome;
+    public Jogador(string nome){
+        this.nome = nome;
+        energia = 100;
+    }
+    public int getEnergia(){
+        return energia;
+    }
+    public string getNome(){
+        return nome;
+    }
+    public void setEnergia(int e){
+        if (e < 0){
+            if (energia + e <0){
+                energia = 0;
+            }else{
+                energia+=e;
+            }
+        }else if(e > 0){
+            if (energia + e > 100){
+                energia = 100;
+            }else{
+                energia+=e;
+            }
+        }
+    }
+}
+class Program{
+
+    static void Main(){
+        Jogador j1 = new Jogador("Pedro");
+
+        j1.setEnergia(-20);
+        j1.setEnergia(40);
+        Console.WriteLine(j1.getNome());
+        Console.WriteLine(j1.getEnergia());
+        Console.ReadKey();
+    }
+
+}
